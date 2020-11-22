0530e2f1f3c3dea8c2404fbc0771a1bd6c46b132
Linkeer365 <564142445@qq.com>
2020-11-23 01:07:05 +0800

    使用proxy_pool进行管理...最新成果比原先多了50条，仅此而已...

--- get_isbn_ssid_pack.py
+++ get_isbn_ssid_pack.py
@@ -2,6 +2,10 @@ import os
 import sys
 import re
 
+import subprocess
+
+import redis
+
 from PIL import Image
 
 import time
@@ -20,28 +24,134 @@ xls_path=r"D:\get_isbn_ssid_pack\publisher_identifiers.xlsx"
 
 ucdrs_url="http://book.ucdrs.superlib.net/search?sw="
 
-ssid_pack_path=r"D:\get_isbn_ssid_pack\ssid_packs.txt"
+ssid_pack_path=r"D:\AllDowns\ssid_packs\ssid_packs.txt"
 
 isbn_exist_error_path=r"D:\get_isbn_ssid_pack\isbn_exist_error.txt"
 
 isbn_after_verify_path=r"D:\get_isbn_ssid_pack\after_verify.txt"
 
+ua_list = [ 'Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)',
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2866.71 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582',
+            'Mozilla/5.0 (compatible; U; ABrowse 0.6; Syllable) AppleWebKit/420+ (KHTML, like Gecko)',
+           'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; Acoo Browser 1.98.744; .NET CLR   3.5.30729)',
+           'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; Acoo Browser; GTB5; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;   SV1) ; InfoPath.1; .NET CLR 3.5.30729; .NET CLR 3.0.30618)',
+           'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; Acoo Browser; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; Avant Browser)',
+           'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1;   .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
+           'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; GTB5; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; Maxthon; InfoPath.1; .NET CLR 3.5.30729; .NET CLR 3.0.30618)',
+           'Mozilla/4.0 (compatible; Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; Acoo Browser 1.98.744; .NET CLR 3.5.30729); Windows NT 5.1; Trident/4.0)',
+           'Mozilla/4.0 (compatible; Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB6; Acoo Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727); Windows NT 5.1; Trident/4.0; Maxthon; .NET CLR 2.0.50727; .NET CLR 1.1.4322; InfoPath.2)',
+           ]
+
+ua_list2=[  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+           "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+           "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+           "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+           "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+           "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+           "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+           "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+           "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+           "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+           "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+           "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+           "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+           "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+           "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+           "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+           "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"]
+
+proxypool_url = 'http://127.0.0.1:5555/random'
+
+ua_idx=0
+
 headers={
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
+    "User-Agent": ua_list2[ua_idx]
 }
 
 yzm_img_path=r"D:\get_isbn_ssid_pack\yzm.png"
 
-s=requests.session()
+# s=requests.session()
 
-def is_isbn_exist(s,isbn):
+# redis login
+
+def redis_login():
+    # import redis
+    pool = redis.ConnectionPool (decode_response=True)
+    db = redis.Redis (connection_pool=pool, host="localhost", port=6379, password='xm111737')
+    print("redis login!")
+
+redis_login()
+
+# boot the proxypool
+
+proxypool_path=r"D:\get_isbn_ssid_pack\ProxyPool\run.py"
+
+boot_proxy_comm=f"python \"{proxypool_path}\""
+
+# os.popen(boot_proxy_comm)
+
+# https://stackoverflow.com/questions/546017/how-do-i-run-another-script-in-python-without-waiting-for-it-to-finish
+# 设置为后台运行...
+
+subprocess.Popen([sys.executable,proxypool_path],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+time.sleep(5)
+
+proxy_status=requests.get(proxypool_url).status_code
+
+if proxy_status==200:
+    print("proxypool boot!")
+
+def get_random_proxy():
+    """
+    get random proxy from proxypool
+    :return: proxy
+    """
+
+    proxy_str=requests.get(proxypool_url).text
+
+    assert ":" in proxy_str
+
+    proxy=proxy_str.strip()
+
+    print("proxy change!")
+    print("proxy:",proxy)
+
+    return proxy
+
+
+def is_isbn_exist(s,isbn,proxy):
+
+    # proxy = "http://" + proxy
+
+    proxies = { 'http': proxy,
+                "https": proxy
+                }
 
     check_str=" 0 种"
 
     assert isinstance(isbn,str)
     url=ucdrs_url+isbn
-    page_text=s.get(url,headers=headers).text
-    time.sleep(3)
+    try:
+        # page_text=s.get(url,headers=headers,proxies=proxies,timeout=120).text
+        page_text=s.get(url,headers=headers,proxies=proxies,timeout=60).text
+    except requests.exceptions.Timeout:
+        print("Timeout!")
+    except requests.exceptions.RequestException:
+        print("Phase1: Connection Error!")
+
+        proxy=get_random_proxy()
+        # s=requests.session()
+
+        s.cookies.clear()
+
+        isbn(s,isbn,proxy)
+
+    # time.sleep(3)
     html=etree.HTML(page_text)
 
     found_zero_patt="//div[@id='searchinfo']/b//text()"
@@ -55,12 +165,46 @@ def is_isbn_exist(s,isbn):
         else:
             return True
     else:
-        with open(isbn_exist_error_path,"a",encoding="utf-8") as f:
-            f.write(f"isbn: {isbn}\n")
-            f.write(page_text)
-            f.write("\n")
+        # with open(isbn_exist_error_path,"a",encoding="utf-8") as f:
+        #     f.write(f"isbn: {isbn}\n")
+        #     f.write(page_text)
+        #     f.write("\n")
         
         print("capcha")
+
+        global ua_idx
+
+        ua_idx+=1
+
+        if ua_idx==len(ua_list):
+            ua_idx=0
+
+        # assert ua_idx<=len(ua_list)-1
+
+        # global headers
+
+        # headers["User-Agent"]=ua_list2[ua_idx]
+
+        # print("ua now:",ua_list2[ua_idx])
+
+        # print("sleep for 60s...")
+
+        # time.sleep(60)
+
+        # s=requests.session()
+
+        s.cookies.clear()
+
+        # 紧急切换proxy
+
+        proxy=get_random_proxy()
+
+        # headers={ua_list[ua_idx]}
+
+        is_isbn_exist(s,isbn,proxy)
+
+
+
 
         # 验证过程是逃不掉的！
         
@@ -73,74 +217,76 @@ def is_isbn_exist(s,isbn):
 
         # is_isbn_exist(s,isbn)
 
-        huanyizhang=0
-        cnt=0
+        # 验证码片段
 
-        while huanyizhang==1 or cnt==0:
-
-            cnt+=1
-            huanyizhang=0
-
-            yzm_url="http://book.ucdrs.superlib.net/antispiderShowVerify.ac"
-            yzm_page_text=s.get(yzm_url,headers=headers).text
-            yzm_html=etree.HTML(yzm_page_text)
-            yzm_img_link_patt="//span[@class='yzmImg']/img//@src"
-
-            yzm_img_link_head="http://book.ucdrs.superlib.net"
-
-            yzm_img_link_tail=yzm_html.xpath(yzm_img_link_patt)[0]
-
-            yzm_img_link=yzm_img_link_head+yzm_img_link_tail
-
-            print("yzm pic link:",yzm_img_link)
-
-            yzm_img=s.get(yzm_img_link,headers=headers).content
-
-            with open(yzm_img_path,"wb") as f:
-                f.write(yzm_img)
-
-            img=Image.open(yzm_img_path)
-            img.show()
-
-            yzm=input("Your input:")
-
-            if yzm=="":
-                # 回车键就是默认换一张
-                huanyizhang=1
-                continue
-            payload={'ucode':yzm}
-
-            process_yzm_url=yzm_img_link_head+"/processVerify.ac?ucode=asyc"
-
-            checker_text=s.get(process_yzm_url,headers=headers,params=payload).text
-
-            with open(isbn_after_verify_path,"a",encoding="utf-8") as f:
-                    f.write(f"isbn:{isbn}")
-                    f.write("\n")
-                    f.write(checker_text)
-                    f.write("\n")
-
-
-
-            r=requests.get(url,headers=headers)
-
-            checker_url=r.url
-            checker_text=r.text
-
-            print("checker url:",checker_url)
-
-            if not 'antispiderShowVerify.ac' in checker_url:
-                
-                print('yanzhengtongguo')
-
-                time.sleep(60)
-
-                s=requests.session()
-
-                is_isbn_exist(s,isbn)
-            else:
-                print("gan!")
-                huanyizhang=1
+        # huanyizhang=0
+        # cnt=0
+        #
+        # while huanyizhang==1 or cnt==0:
+        #
+        #     cnt+=1
+        #     huanyizhang=0
+        #
+        #     yzm_url="http://book.ucdrs.superlib.net/antispiderShowVerify.ac"
+        #     yzm_page_text=s.get(yzm_url,headers=headers).text
+        #     yzm_html=etree.HTML(yzm_page_text)
+        #     yzm_img_link_patt="//span[@class='yzmImg']/img//@src"
+        #
+        #     yzm_img_link_head="http://book.ucdrs.superlib.net"
+        #
+        #     yzm_img_link_tail=yzm_html.xpath(yzm_img_link_patt)[0]
+        #
+        #     yzm_img_link=yzm_img_link_head+yzm_img_link_tail
+        #
+        #     print("yzm pic link:",yzm_img_link)
+        #
+        #     yzm_img=s.get(yzm_img_link,headers=headers).content
+        #
+        #     with open(yzm_img_path,"wb") as f:
+        #         f.write(yzm_img)
+        #
+        #     img=Image.open(yzm_img_path)
+        #     img.show()
+        #
+        #     yzm=input("Your input:")
+        #
+        #     if yzm=="":
+        #         # 回车键就是默认换一张
+        #         huanyizhang=1
+        #         continue
+        #     payload={'ucode':yzm}
+        #
+        #     process_yzm_url=yzm_img_link_head+"/processVerify.ac?ucode=asyc"
+        #
+        #     checker_text=s.get(process_yzm_url,headers=headers,params=payload).text
+        #
+        #     with open(isbn_after_verify_path,"a",encoding="utf-8") as f:
+        #             f.write(f"isbn:{isbn}")
+        #             f.write("\n")
+        #             f.write(checker_text)
+        #             f.write("\n")
+        #
+        #
+        #
+        #     r=requests.get(url,headers=headers)
+        #
+        #     checker_url=r.url
+        #     checker_text=r.text
+        #
+        #     print("checker url:",checker_url)
+        #
+        #     if not 'antispiderShowVerify.ac' in checker_url:
+        #
+        #         print('yanzhengtongguo')
+        #
+        #         time.sleep(60)
+        #
+        #         s=requests.session()
+        #
+        #         is_isbn_exist(s,isbn)
+        #     else:
+        #         print("gan!")
+        #         huanyizhang=1
 
 
 
@@ -148,7 +294,7 @@ def is_isbn_exist(s,isbn):
 # is_isbn_exist(bad_isbn)
 # sys.exit(0)
 
-def get_ssid_packs(s,isbn,is_exist=True):
+def get_ssid_packs(s,isbn,proxy,is_exist=True):
     '''
     pack format: (isbn,ssid,ssid_info,ucdrs_link)
 
@@ -156,17 +302,36 @@ def get_ssid_packs(s,isbn,is_exist=True):
     :param is_exist:
     :return: packs
     '''
+
+    proxies = {'http': 'http://' + proxy}
+
     assert isinstance (isbn, str)
 
-    max_page_num=10
+    # max_page_num=10
+    max_page_num=1
 
     packs = []
 
     for page_num in range(1,max_page_num+1):
         url = ucdrs_url + isbn + f"&Pages={page_num}"
-        page_text = s.get (url, headers=headers).text
+        try:
+            page_text = s.get (url, headers=headers,proxies=proxies,timeout=60).text
+        except requests.exceptions.Timeout:
+            print("Timeout!")
+        except requests.exceptions.RequestException:
+            print("Phase2: Connection Error!")
 
-        time.sleep(3)
+            proxy=get_random_proxy()
+
+            s.cookies.clear()
+
+            get_ssid_packs(s,isbn,proxy)
+
+
+
+
+
+        # time.sleep(3)
 
         html = etree.HTML (page_text)
 
@@ -414,6 +579,9 @@ def main():
     except pymysql.err.OperationalError:
         pass
 
+    s=requests.session()
+    proxy = get_random_proxy ()
+
     for publisher_identifier in res:
 
         all_packs = []
@@ -422,14 +590,33 @@ def main():
 
         max_ti_len=get_max_ti_len(publisher_identifier)
 
+        cnt=0
+
         for num in range(0,10**max_ti_len):
             full_ti=get_full_ti_str(num,max_ti_len)
             isbn13=ISBN13(state_identifier=state_identifier,publish_identifier=publisher_identifier,
                           title_identifier=full_ti)
             isbn=isbn13.get_full_without_hyphen()
-            is_exist=is_isbn_exist(s,isbn)
+
+            
+            if cnt==15:
+                
+                # 每15次就大更新一次
+
+                # headers["User-Agent"]=ua_list2[ua_idx]
+                proxy=get_random_proxy()
+
+                # s=requests.session()
+
+                s.cookies.clear()
+
+                # time.sleep(20)
+                # print("now we sleep for 20s...")
+                cnt=0
+            is_exist=is_isbn_exist(s,isbn,proxy)
+            cnt+=1
             if is_exist:
-                packs=get_ssid_packs(s,isbn)
+                packs=get_ssid_packs(s,isbn,proxy)
                 all_packs.extend(packs)
         insert_packs_sql2=  f"INSERT INTO {tb_name2} " \
                             f"(isbn,ssid,ssid_info,ucdrs_link)" \
